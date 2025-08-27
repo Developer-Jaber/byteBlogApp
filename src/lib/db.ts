defc4b556496f85b4c5637827c3b593d340da36d
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2025-08-28 01:57:53 +0400

    feat: add Prisma integration with SQLite database
    - Created a new .env file to store the DATABASE_URL.
    - Updated .gitignore to exclude .env files and generated Prisma files.
    - Added Prisma client and Prisma CLI as dependencies in package.json.
    - Created a SQLite database file at prisma/dev.db.
    - Defined a Prisma schema with a User model in schema.prisma.
    - Added migration files for creating the User table and unique index on email.
    - Implemented a database connection in db.ts.
    - Updated the Home page to fetch and display the first user from the database.
    - Modified tsconfig.json to include new path alias for Prisma client.

---
+++ src/lib/db.ts
@@ -0,0 +1,18 @@
+
+import { PrismaClient } from "$/generated/prisma";
+
+
+
+const prismaClientSignleton = () => {
+    return new PrismaClient()
+}
+
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSignleton>;
+} & typeof global;
+
+const db = globalThis.prismaGlobal ?? prismaClientSignleton();
+
+export default db;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
